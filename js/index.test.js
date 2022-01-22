c4fd6b25e8df791cb0a35b33d0ed9c7ca0985e2d
Albert Domínguez <albertdc1984@gmail.com>
2022-01-22 12:24:42 +0100

    Added styles to game board

--- js/index.test.js
+++ js/index.test.js
@@ -1,3 +1,19 @@
+const htmlGenerator = (parameterRow, parameterColumn) => {
+  const gameBoard = document.querySelector(".game-container");
+  const board = document.createElement("table");
+  for (let i = 0; i < parameterRow; i++) {
+    const row = document.createElement("tr");
+    for (let j = 0; j < parameterColumn; j++) {
+      const column = document.createElement("td");
+      column.setAttribute("dead");
+      row.appendChild(column);
+    }
+    board.appendChild(row);
+  }
+  gameBoard.appendChild(board);
+};
+htmlGenerator(50, 50);
+
 const gameTable = (numberOfColumns, numberOfRows) => {
   const nestedArray = [];
   for (let i = 0; i < numberOfRows; i++) {
