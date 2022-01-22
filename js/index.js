67d71af4377687aff8e07c48de08c61dc650bcb1
Albert Domínguez <albertdc1984@gmail.com>
2022-01-22 21:58:48 +0100

    Small progresses with game logics and few fixes to the generation of board

--- js/index.js
+++ js/index.js
@@ -1,19 +1,49 @@
+const gameTable = (numberOfColumns, numberOfRows) => {
+  const nestedArray = [];
+  for (let i = 0; i < numberOfRows; i++) {
+    nestedArray.push([]);
+  }
+
+  for (let i = 0; i < numberOfColumns; i++) {
+    for (let j = 0; j < numberOfRows; j++) {
+      nestedArray[j].push([0]);
+    }
+  }
+  console.table(nestedArray);
+  return nestedArray;
+};
+gameTable();
+
 const htmlGenerator = (parameterRow, parameterColumn) => {
   const gameBoard = document.querySelector(".game-container");
-  const board = document.createElement("table");
+  const board = document.createElement("div");
+  board.className = "boardDiv";
+  let rowCount = 0;
+  let cellCount = 0;
   let ids = 0;
+  let row;
+  let cell;
   for (let i = 0; i < parameterRow; i++) {
-    const row = document.createElement("tr");
+    row = document.createElement("div");
+    row.className = `rowDiv ${i}`;
+    rowCount++;
     for (let j = 0; j < parameterColumn; j++) {
       ids++;
-      const column = document.createElement("td");
-      column.setAttribute("dead", true);
-      column.classList.add(ids);
-      row.appendChild(column);
+      cellCount++;
+      cell = document.createElement("div");
+      cell.className = `cellDiv ${i}`;
+      cell.classList.add(ids);
+      row.appendChild(cell);
     }
     board.appendChild(row);
   }
   gameBoard.appendChild(board);
+  return { rowCount, cellCount };
 };
 
 window.onload = () => htmlGenerator(40, 40);
+
+const start = () => {
+  htmlGenerator();
+};
+window.onload = start();
--- js/index.test.js
+++ js/index.test.js
@@ -1,21 +1,3 @@
-const htmlGenerator = (parameterRow, parameterColumn) => {
-  const gameBoard = document.querySelector(".game-container");
-  const board = document.createElement("table");
-  const ids = document.setAttribute("id");
-
-  for (let i = 0; i < parameterRow; i++) {
-    const row = document.createElement("tr");
-    for (let j = 0; j < parameterColumn; j++) {
-      const column = document.createElement("td");
-      column.setAttribute("id", ids + 1);
-      row.appendChild(column);
-    }
-    board.appendChild(row);
-  }
-  gameBoard.appendChild(board);
-};
-htmlGenerator(50, 50);
-
 const gameTable = (numberOfColumns, numberOfRows) => {
   const nestedArray = [];
   for (let i = 0; i < numberOfRows; i++) {
@@ -24,21 +6,36 @@ const gameTable = (numberOfColumns, numberOfRows) => {
 
   for (let i = 0; i < numberOfColumns; i++) {
     for (let j = 0; j < numberOfRows; j++) {
-      nestedArray[j].push([]);
+      nestedArray[j].push([0]);
     }
   }
   console.table(nestedArray);
   return nestedArray;
 };
 
+const gameFive = gameTable(5, 5);
+console.log(gameFive);
+
+const logics = () => {
+  for (let i = 1; i < gameFive[0].length; i++) {
+    for (let j = 1; gameFive[0][0].length; j++) {
+      if (gameFive[i][j + 1] === 0 && gameFive[i - 1][j] === 0) {
+        gameFive[i][j] = 1;
+      }
+    }
+  }
+  return console.table(gameFive);
+};
+logics();
+console.table(gameTable(3, 3));
 describe("Given a tableGenerator funtion", () => {
   describe("When it recieves the value of 3 and 3", () => {
     test("Then is hould return a table of 3 rows & 3 columns", () => {
       // Arrange
       const expectedResult = [
-        [[], [], []],
-        [[], [], []],
-        [[], [], []],
+        [[0], [0], [0]],
+        [[0], [0], [0]],
+        [[0], [0], [0]],
       ];
       const columnNumber = 3;
       const rowNumber = 3;
