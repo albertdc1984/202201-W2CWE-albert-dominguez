1cc1b26fa3caada7facf946196ae85a55fad2c63
Albert Domínguez <albertdc1984@gmail.com>
2022-01-23 16:04:12 +0100

    Created relationship between html cells & array cells. alive=1/dead=0

--- js/index.js
+++ js/index.js
@@ -1,14 +1,7 @@
-function cellStatus() {
-  if (this.className === "alive") {
-    this.setAttribute("class", "dead");
-  } else {
-    this.setAttribute("class", "alive");
-  }
-}
+const rows = 10;
+const columns = 10;
 
 const htmlGenerator = (parameterRow, parameterColumn) => {
-  const rows = parameterRow;
-  const cols = parameterColumn;
   const gameBoard = document.querySelector(".game-container");
   const board = document.createElement("div");
   board.className = "boardDiv";
@@ -24,15 +17,15 @@ const htmlGenerator = (parameterRow, parameterColumn) => {
       cell.setAttribute("id", `${i}_${j}`);
       cell.setAttribute("class", "dead");
       cell.classList.add(ids);
-      row.appendChild(cell);
       cell.addEventListener("click", cellStatus);
+      row.appendChild(cell);
     }
     board.appendChild(row);
   }
-  gameBoard.appendChild(board);
-  return gameBoard;
-};
 
+  gameBoard.appendChild(board);
+  return board;
+};
 const gameTable = (numberOfColumns, numberOfRows) => {
   const nestedArray = [];
   for (let i = 0; i < numberOfRows; i++) {
@@ -41,12 +34,27 @@ const gameTable = (numberOfColumns, numberOfRows) => {
 
   for (let i = 0; i < numberOfColumns; i++) {
     for (let j = 0; j < numberOfRows; j++) {
-      nestedArray[j].push([0]);
+      nestedArray[i].push(0);
     }
   }
   console.table(nestedArray);
   return nestedArray;
 };
-gameTable();
+const nestedArray1 = gameTable(10, 10);
+function cellStatus() {
+  const location = this.id.split("_");
+  const rowLocation = Number(location[0]);
+  const columnLocation = Number(location[1]);
 
-window.onload = () => htmlGenerator(40, 40);
+  if (this.className === "alive") {
+    this.setAttribute("class", "dead");
+    nestedArray1[rowLocation][columnLocation] = 0;
+  } else {
+    this.setAttribute("class", "alive");
+    nestedArray1[rowLocation][columnLocation] = 1;
+  }
+}
+window.onload = () => {
+  htmlGenerator(rows, columns);
+  gameTable(rows, columns);
+};
